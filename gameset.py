d8f3ec97ca6e4cc4f013b0c7cb3b4534edf66636
AkaSan242 <Lucaka@icloud.com>
2021-09-01 13:19:00 +0200

    Gameset application

--- gameset.py
+++ gameset.py
@@ -1,20 +1,10 @@
 """Main script of gameset test"""
 
 from controllers.controller import Controller
+from views.tournamentview import tournament_main_page
 
-gameset_players = []
-gameset_tournament = []
-gameset_match_list = []
-tournament_players = []
-match_list = []
-tour_list = []
-tournament_match = [] 
-
-
-game = Controller(gameset_players, gameset_tournament,
-                  tournament_players, match_list,
-                  tour_list, tournament_match_list=tournament_match, gameset_match_list=gameset_match_list)
+game = Controller()
 
 print("BIENVENUE DANS GAMESET !")
-game.tournament_main_page()
+tournament_main_page()
 game.choose()
